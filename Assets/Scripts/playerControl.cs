6f5ead1acdef8e2f255b851f6369f73714bc4804
Mars Paton <pumpkinpotato5@gmail.com>
2021-02-18 22:40:47 -0600

    Added player and basic physics
    Created the player, gave it a sprite and the ability to move and jump, and set everything up for the draft stages

---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public float speed;
+    public float speedDivisor;
+    public float jumpForce;
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+    private Rigidbody2D rigidbody2D;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+    private bool is_on_ground;
+    private int jumps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float adjustedSpeed = speed / speedDivisor;
+        float moveX = Input.GetAxis("Horizontal") * adjustedSpeed;
+        float moveY = Input.GetAxis("Vertical");
+
+        if (is_on_ground)
+        {
+            transform.position += new Vector3(moveX, 0f, 0f);
+        } else
+        {
+            transform.position += new Vector3((float)(moveX / 1.5), 0f, 0f);
+        }
+        if (Input.GetButtonDown("Jump") && (is_on_ground || jumps != 2))
+        {
+            Vector2 jumpF = new Vector2(0, jumpForce);
+            rigidbody2D.AddForce(jumpF);
+            jumps++;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CollisionIsWithGround(collision))
+        {
+            is_on_ground = true;
+            jumps = 0;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!CollisionIsWithGround(collision))
+        {
+            is_on_ground = false;
+            if (jumps == 0 && jumps != 2)
+            {
+                jumps = 1;
+            }
+        }
+    }
+    private bool CollisionIsWithGround (Collision2D collision)
+    {
+        bool is_with_ground = false;
+        foreach(ContactPoint2D c in collision.contacts)
+        {
+            Vector2 collision_direction_vector = c.point - rigidbody2D.position;
+            if(collision_direction_vector.y < 0)
+            {
+                is_with_ground = true;
+            }
+        }
+        return is_with_ground;
+    }
+}
